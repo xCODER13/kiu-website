a7050429f4b017e4846801c59f9b946627ed598e
xCODER13 <xamrayevxurshidjon@gmail.com>
2026-04-11 16:08:15 +0500

    fix document icons

--- src/pages/Documents.jsx
+++ src/pages/Documents.jsx
@@ -33,7 +33,7 @@ const DOCS = [
     title: "Odob-axloq kodeksi", 
     desc: "Universitet odob-axloq kodeksi", 
     url: '/docs/Odob-axloq-kodeksi.pdf', 
-    icon: <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/></svg> 
+    icon: <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg> 
   },
   
 ]
