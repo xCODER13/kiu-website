e2221c856fd3a2f5bb3d96172c94f6794810f986
xCODER13 <xamrayevxurshidjon@gmail.com>
2026-04-18 17:16:46 +0500

    fix: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        }
-      }
-    }
-  }
 })
