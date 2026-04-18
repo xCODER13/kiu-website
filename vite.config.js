491ad128790c2b0ad78b4c8de56cf57c78605dc6
xCODER13 <xamrayevxurshidjon@gmail.com>
2026-04-18 18:46:39 +0500

    fix: manualChunks as function for vite 8

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
         }
       }
     }
